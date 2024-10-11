3abba7b0a46d4be885b21347c56cc69eaf89c107
Ronan Abhamon <ronan.abhamon@vates.fr>
2024-10-11 17:51:43 +0200

    Use abstract properties for child classes if necessary
    The goal here is to detect class variables that are not defined in child classes.
    
    In this example, there are two ways to declare these variables, the first approach
    is short but does not clearly indicate that we are overriding a property.
    The second is clearly more explicit but heavier.
    
    ```python
    
    from typing import ClassVar, override
    
    from abc import ABC, abstractmethod
    
    class KObjectAbstract(ABC):
        @abstractmethod
        @property
        def SYSFS_CLASSTYPE(self) -> int:
            pass
    
    class KChild(KObjectAbstract):
        SYSFS_CLASSTYPE: ClassVar[int] = 42
    
    class KChild2(KObjectAbstract):
        @override
        @property
        def SYSFS_CLASSTYPE(self) -> int:
            return 42
    
    class KBadChild(KObjectAbstract):
        pass
    
    x = KChild()
    x2 = KChild2()
    y = KBadChild() # error: Cannot instantiate abstract class "KBadChild" with abstract attribute "SYSFS_CLASSTYPE"  [abstract]
    ```
    
    Why did we choose the KChild2 solution for this commit?
    
    In this example we can instantiate KChild without warning when "override" decorators are enabled and it's clearly not good.
    A class variable is not supposed to be override, hence the use of an abstract property method in the parent.
    And that when inheriting we should not use a variable class in the KChild case.
    
    Concerning unmodified class variables in child classes, the correct solution is to always use this syntax:
    ```
    MY_VAR: ClassVar[TYPE] = VALUE
    ```
    
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

--- sm_typing/__init__.py
+++ sm_typing/__init__.py
@@ -1,2 +1,13 @@
 import typing
 from typing import *
+
+if not hasattr(typing, 'override'):
+    def override(method): # type: ignore
+        def wrapper(cls):
+            try:
+                # Set internal attr `__override__` like described in PEP 698.
+                method.__override__ = True
+            except (AttributeError, TypeError):
+                pass
+            return method
+        return wrapper
