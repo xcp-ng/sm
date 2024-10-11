de0611f0bf1329aaa6accbc5a340bd073cf5d696
Ronan Abhamon <ronan.abhamon@vates.fr>
2024-10-11 17:51:25 +0200

    Use static analysis tool (mypy)
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -1,3 +1,5 @@
+from sm_typing import List
+
 import errno
 import signal
 import unittest
@@ -31,7 +33,7 @@ class FakeException(Exception):
 
 
 class FakeUtil:
-    record = []
+    record: List[str] = []
 
     def log(input):
         FakeUtil.record.append(input)
