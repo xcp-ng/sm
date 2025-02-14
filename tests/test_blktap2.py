ec6874c98492cf1f074108dab9b2871eb0e726da
Damien Thenot <damien.thenot@vates.tech>
2025-02-14 17:01:16 +0100

    feat(qcow2): Add coalesce with call to tapdisk
    The online coalesce allow to coalesce a QCOW2 while the VDI is inuse safely.
    We use newly added commit and query in `blktap2.TapCtl`.
    We handle relink of other children and let tapdisk do the one it knows about.
    We set children RW of a LVM VDI, children need to be writable
    so tapdisk can change parent while doing commit job.
    
    We change LV activation for qcow2util to use LVMCache for better support of LVMSRs.
    
    We have a mechanism for stopping the coalesce when activating a VDI.
    It's necessary to avoid corrupting the VDI while having it activated and coalescing the chain.
    But also to have activation time being short.
    The coalesce would be interrupted and started again after the activation has been done.
    Then the mechanism will try to do an online coalesce on the newly created tapdisk.
    In case we close the tapdisk, the coalesce would stop automatically and fail,
    we would then try to do it offline.
    This mechanism work by creating a file in `/var/run/nonpersistent/sm/` that is
    cleared by the `on-slave` function on the master. If it's cleared, the GC will
    abort then delete the file to notify it's done. Then the plugin will return and
    the activation can continue.
    To know that a coalesce is in progress, the activation in `blktap2.py` check
    for `coalesce_` journals on the chain.
    
    The GC worker thread need to be the one doing the cancel call since multiple
    threads using the same session can't call a plugin at the same time.
    We use a custom signal handler to raise an expection that we catch
    then do the call to cancel the coalesce in the worker thread.
    
    Before, we would let the running_file until after the relink.
    Sometime, the VDI activation code in blktap2.py would wait on coalesce
    being stopped while the GC code would be waiting for the activation
    being done to continue with the relink, causing a deadlock.
    We remove the coalesce running file before the relink so that it
    continue as before, the activation will proceed then the relink will
    run and refresh the tapdisk with the new parent.
    For the relinking, we have a condition to not add the relinking key to the
    leafs of the chain used for online coalesce.
    Tapdisk will have done the rebase itself and the scan before doing the
    relink would drop the VDIs from the children list and the tag would never be removed.
    It would then block the vdi_activate that check if relink is happening before continuing.
    
    If the VDI is being coalesced, the VDI activation would want to interrupt it.
    But the LVMSR coalesce needs the lvchange-p lock of the SR.
    We need to check for chain coalesce before taking the lock.
    
    Signed-off-by: Damien Thenot <damien.thenot@vates.tech>

--- tests/test_blktap2.py
+++ tests/test_blktap2.py
@@ -266,7 +266,8 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
-    def test_activate(self, mock_tapdisk, mock_nbd_link, mock_backend,
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
+    def test_activate(self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
                       mock_attach, mock_this_host, mock_sleep):
         """
         Test blktap2.VDI.activate, no cache, RW, success
@@ -293,8 +294,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_relink_retry(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         Test blktap2.VDI.activate, relinking, retry 1, success
@@ -319,8 +321,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_pause_retry(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         Test blktap2.VDI.activate, paused, retry 1, success
@@ -344,8 +347,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_paused_while_tagging(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         Test blktap2.VDI.activate, paused, while tagging, success
@@ -375,8 +379,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_relink_while_tagging(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         Test blktap2.VDI.activate, relinking, while tagging, retry 1, success
@@ -406,8 +411,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_ro_already_activating_retry(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         If we're activating for read-only access, with someone else (let's
@@ -442,8 +448,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_rw_already_activating_fail(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         If we're activating for read-write access, with someone else (let's
