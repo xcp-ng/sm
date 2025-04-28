abffda512d27b4a497184328c2431da493da4868
Mathieu Labourier <mathieu.labourier@vates.tech>
2025-04-28 13:45:01 +0200

    fix: assume the cache is valid
    Signed-off-by: Mathieu Labourier <mathieu.labourier@vates.tech>

--- drivers/linstorvolumemanager.py
+++ drivers/linstorvolumemanager.py
@@ -231,33 +231,20 @@ def write_controller_uri_cache(uri):
         ))
 
 
-def is_controller_uri_valid(uri):
-    PLUGIN_CMD = 'hasControllerRunning'
-
-    if not uri:
-        return False
-    address = uri.removeprefix("linstor://")
-    session = util.timeout_call(10, util.get_localAPI_session)
-    for host_ref, host_record in session.xenapi.host.get_all():
-        if host_record.get('address', '') != address:
-            continue
-        return util.strtobool(
-            session.xenapi.host.call_plugin(host_ref, PLUGIN, PLUGIN_CMD, {})
-        )
-    return False
-
-
-def get_controller_uri():
-    uri = get_cached_controller_uri()
-    if uri and is_controller_uri_valid(uri):
-        return uri
+def build_controller_uri_cache():
     for _ in range(10):
         uri = _get_controller_uri()
-        if uri and is_controller_uri_valid(uri):
+        if uri:
             write_controller_uri_cache(uri)
             return uri
         time.sleep(1)
+    return None
 
+def get_controller_uri():
+    uri = get_cached_controller_uri()
+    if not uri:
+        uri = build_controller_uri_cache()
+    return uri
 
 def get_controller_node_name():
     PLUGIN_CMD = 'hasControllerRunning'
