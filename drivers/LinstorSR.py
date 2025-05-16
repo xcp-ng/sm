1dbb24872171b12578ecf769c223431b240219c7
Mathieu Labourier <mathieu.labourier@vates.tech>
2025-05-16 16:19:10 +0200

    style(LinstorSR): rename linstorvolumemanager import
    Signed-off-by: Mathieu Labourier <mathieu.labourier@vates.tech>

--- drivers/LinstorSR.py
+++ drivers/LinstorSR.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from drivers.linstorvolumemanager import write_controller_uri_cache
+from linstorvolumemanager import write_controller_uri_cache
 from sm_typing import Optional, override
 
 from constants import CBTLOG_TAG
