f81ae74662782489d6333dac06f563ca4ded0878
Damien Thenot <damien.thenot@vates.tech>
2025-06-02 09:09:54 +0200

    fix(on_slave.py): `is_open` must use lvm SR type due to LVHD/LVM refactor
    Signed-off-by: Damien Thenot <damien.thenot@vates.tech>
    Co-authored-by: Ronan Abhamon <ronan.abhamon@vates.tech>

--- tests/test_on_slave.py
+++ tests/test_on_slave.py
@@ -84,7 +84,7 @@ class Test_on_slave_is_open(unittest.TestCase):
                                        'vdiUuid': vdi_uuid,
                                        'srRef': 'opaqueref:sr_mine'
                                    })
-        self.mock_sr.driver.assert_called_once_with('lvhd')
+        self.mock_sr.driver.assert_called_once_with('lvm')
         self.assertEqual('True', is_open)
 
     def test_is_open_false(self):
