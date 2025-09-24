c594055d8426e6fb7d83419950b62fa006f5f622
Damien Thenot <damien.thenot@vates.tech>
2025-09-24 17:15:41 +0200

    Mock check_journal_coalesce for VDI activation in test_blktap2
    Signed-off-by: Damien Thenot <damien.thenot@vates.tech>

--- tests/test_blktap2.py
+++ tests/test_blktap2.py
@@ -260,8 +260,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_relink_retry(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         Test blktap2.VDI.activate, relinking, retry 1, success
@@ -286,8 +287,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_pause_retry(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         Test blktap2.VDI.activate, paused, retry 1, success
@@ -311,8 +313,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_paused_while_tagging(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         Test blktap2.VDI.activate, paused, while tagging, success
@@ -342,8 +345,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_relink_while_tagging(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         Test blktap2.VDI.activate, relinking, while tagging, retry 1, success
@@ -373,8 +377,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_ro_already_activating_retry(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         If we're activating for read-only access, with someone else (let's
@@ -409,8 +414,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
+    @mock.patch('blktap2.VDI._check_journal_coalesce_chain', autospec=True)
     def test_activate_rw_already_activating_fail(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_checkjournalcoalesce, mock_tapdisk, mock_nbd_link, mock_backend,
             mock_attach, mock_this_host, mock_sleep):
         """
         If we're activating for read-write access, with someone else (let's
