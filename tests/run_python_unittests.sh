1dd1d10499f7938faae5352f227e1ed461121b0d
Damien Thenot <damien.thenot@vates.tech>
2026-09-23 15:17:34 +0200

    Remove parts of `CP-312848`
    Remove changes on `tests/run_python_unittests.sh` introduced by
    `CP-312848: allow tapdisk-pause.Pause to be idempotent when safe`
    (f998adba146597b0166f17664adaeb2b4d376a9e)
    
    This was removed from the patch in Citrix SRPM: sm-3.2.12-25.xs8.src.rpm
    
    The directory `scripts/plugins/` doesn't exist.
    
    Signed-off-by: Damien Thenot <damien.thenot@vates.tech>

--- tests/run_python_unittests.sh
+++ tests/run_python_unittests.sh
@@ -20,9 +20,9 @@ fi
 
 (
     cd "$SMROOT"
-    PYTHONPATH="$SMROOT/mocks:$SMROOT/drivers:$SMROOT/misc/fairlock:$SMROOT/scripts/plugins" \
+    PYTHONPATH="$SMROOT/mocks:$SMROOT/drivers:$SMROOT/misc/fairlock" \
         $COVERAGE run --branch \
-            --source="$SMROOT/drivers,$SMROOT/tests,$SMROOT/misc/fairlock,$SMROOT/scripts/plugins" \
+            --source="$SMROOT/drivers,$SMROOT/tests,$SMROOT/misc/fairlock" \
             -m unittest discover -f -s "$TESTS" -p "$FILES" -v
 
     echo "Test coverage"
